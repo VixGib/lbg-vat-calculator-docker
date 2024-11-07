210e9eacd0aad6297fb547810ba7043c4579668d
Vicki Gibbison <vickigibbison@hotmail.com>
2024-11-07 11:55:13 +0000

    added vicki

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <div className='header field'>
-     <h1>Vicki</h1>
+     <h1>Vicki Calc</h1>
       VAT CALCULATOR
       <div className='colour-border'>
         <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
